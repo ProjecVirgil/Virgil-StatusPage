517db5fa939d2912b2238fb7ca952fe1749ba238
Francesco è volevi <deadragon99@outlook.it>
2023-10-30 18:43:04 +0100

    Add group feature in the panel admin

--- backend/status_page/mainapp/admin.py
+++ backend/status_page/mainapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Service,ItemStatus
+from .models import Service,ItemStatus,Feature
 # Register your models here.
 admin.site.register(Service)
 admin.site.register(ItemStatus)
+admin.site.register(Feature)
